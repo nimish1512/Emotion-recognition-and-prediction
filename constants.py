347ecda0904da84dbfd31de43d828cbf3318b9b6
Nimish Ronghe <nimish1512@gmail.com>
2017-05-23 19:31:20 +0530

    Delete constants.py

--- constants.py
+++
@@ -1,9 +0,0 @@
-CASC_PATH = './haarcascade_files/haarcascade_frontalface_default.xml'
-SIZE_FACE = 48
-EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
-SAVE_DIRECTORY = ''
-SAVE_MODEL_FILENAME = 'tfleaarncnn.model'
-SAVE_DATASET_IMAGES_FILENAME = 'data_kike.npy'
-SAVE_DATASET_LABELS_FILENAME = 'labels_kike.npy'
-SAVE_DATASET_IMAGES_TEST_FILENAME = 'data_kike.npy'
-SAVE_DATASET_LABELS_TEST_FILENAME = 'labels_kike.npy'
